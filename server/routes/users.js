1e86958a4766f82f3f2ece0627fca081853b1938
laurence090403 <bridge.atindokpo@epitech.eu>
2023-12-10 14:06:09 +0100

    [feat] : Api implemenetation

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/ui', function(req, res, next) {
   res.send('Users!')
 });
 
