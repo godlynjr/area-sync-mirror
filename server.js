591f399bc5de1d2ab23044cbcc4ca3864ea65e15
Yannick <yannick.keke@epitech.eu>
2023-12-10 01:47:59 +0100

    [add]: user authentication with token generation

---
+++ server.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Utilisez express.json() avant vos routes
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/register', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    
+        const newUser = new User({
+            username: req.body.username,
+            password: hashedPassword,
+        });
+    
+        await newUser.save();
+    
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error', error: error.toString() });
+    }
+});
+
+app.post('/login', async (req, res) => {
+        try {
+               const user = await User.findOne({ username: req.body.username });
+        
+                if (!user) {
+                        return res.status(400).json({ message: 'Invalid username or password' });
+                    }
+            
+                    const validPassword = await bcrypt.compare(req.body.password, user.password);
+            
+                    if (!validPassword) {
+                            return res.status(400).json({ message: 'Invalid username or password' });
+                        }
+   
+                       const token = jwt.sign({ _id: user._id }, 'SECRET_KEY');
+                
+                       res.header('auth-token', token).send({ token });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
