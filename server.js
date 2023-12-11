d7a495a3d5d28303d2d8a046b11e8eac83b8db6c
godlynjr <godlyn.kikissagbe@epitech.eu>
2023-12-11 20:35:11 +0100

    [feat]: adding Docker to repo pt.1

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const bcrypt = require('bcrypt');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Utilisez express.json() avant vos routes
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://mongodb:27017/async', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -22,14 +21,14 @@ app.post('/register', async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    
+
         const newUser = new User({
             username: req.body.username,
             password: hashedPassword,
         });
-    
+
         await newUser.save();
-    
+
         res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
         console.error(error);
@@ -38,27 +37,27 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-        try {
-               const user = await User.findOne({ username: req.body.username });
-        
-                if (!user) {
-                        return res.status(400).json({ message: 'Invalid username or password' });
-                    }
-            
-                    const validPassword = await bcrypt.compare(req.body.password, user.password);
-            
-                    if (!validPassword) {
-                            return res.status(400).json({ message: 'Invalid username or password' });
-                        }
-   
-                       const token = jwt.sign({ _id: user._id }, 'SECRET_KEY');
-                
-                       res.header('auth-token', token).send({ token });
+    try {
+        const user = await User.findOne({ username: req.body.username });
+
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid username or password' });
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({ message: 'Invalid username or password' });
+        }
+
+        const token = jwt.sign({ _id: user._id }, 'SECRET_KEY');
+
+        res.header('auth-token', token).send({ token });
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
     }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
