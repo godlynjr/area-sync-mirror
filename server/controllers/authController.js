b3497b952f684dfaceb606c63333dd4dbc4fa106
Yannick <yannick.keke@epitech.eu>
2023-12-16 13:44:28 +0100

    [feat]: web_autentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -59,4 +59,50 @@ const login = async (req, res) => {
     }
 };
 
+const web = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (user) {
+            if (user.password === '') {
+                // User exists but password is not set, so set the password
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassword = await bcrypt.hash(req.body.password, salt);
+                user.password = hashedPassword;
+                await user.save();
+                
+                const token = jwt.sign({ _id: user._id }, 'SECRET_KEY');
+                res.header('auth-token', token).send({ token });
+            } else {
+                // User exists and password is set, so just check the password
+                const validPassword = await bcrypt.compare(req.body.password, user.password);
+                if (validPassword) {
+                    const token = jwt.sign({ _id: user._id }, 'SECRET_KEY');
+                    res.header('auth-token', token).send({ token });
+                } else {
+                    res.status(400).json({ message: 'Invalid password' });
+                }
+            }
+        } else {
+            // User does not exist, so create the user
+            const username = req.body.email.split('@')[0];
+            
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            
+            const newUser = new User({
+                username: username,
+                email: req.body.email,
+                password: hashedPassword, // Password is set here
+            });
+            await newUser.save();
+            
+            const token = jwt.sign({ _id: newUser._id }, 'SECRET_KEY');
+            res.header('auth-token', token).send({ token });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error', error: error.toString() });
+    }
+};
+
 module.exports = { check_mail, login };
