59db2c187cbc96ddd58a3cb3e8f3a2e7c370e356
Yannick <yannick.keke@epitech.eu>
2023-12-18 13:47:48 +0100

    [feat]: path to about.json file

--- server/controllers/dataInfo.js
+++ server/controllers/dataInfo.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const about_json = async (req, res) => {
     try {
-        res.sendFile('about.json', { root: __dirname });
+        res.sendFile('about.json', { root: "/server/controllers/about.json" });
     } catch {
         console.error(error);
         res.status(500).json({ message: 'Server error', error: error.toString() });
