bdbfc86f60b8e66f3b83db1a3831787217f79394
Jean Luc <jlahoyo@asin.bj>
2023-12-19 18:23:24 +0100

    feat: dashboard and Userlist basis layout

--- Back_office/src/Screens/UserList.js
+++ Back_office/src/Screens/UserList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Sidebar from '../Components/Sidebar';
 
 const UserList = () => {
     return (
-        <>
-        </>
+        <div className='flex'>
+            <div className='basis-[15%] h-[100vh] border'>
+                <Sidebar />
+            </div>
+            <div className='basis-[85%] border'>UserList</div>
+        </div>
     );
 };
 
