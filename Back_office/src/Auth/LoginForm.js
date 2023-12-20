18365970ab8fe115d1e4a6bb03c7611a0c2d981a
Jean Luc <jlahoyo@asin.bj>
2023-12-20 06:05:15 +0100

    feat: login form layout added

--- Back_office/src/Auth/LoginForm.js
+++ Back_office/src/Auth/LoginForm.js
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
+import logo from "../assets/white-logo.png";
 
 const LoginForm = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
-    return (
-        <>
-        </>
-    );
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  return (
+    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0 bg-[#3e3d45]">
+      <div className="md:w-1/3 max-w-sm">
+        <img src={logo} alt="app logo" />
+      </div>
+      <div className="md:w-1/3 max-w-sm">
+        <input
+          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
+          type="text"
+          placeholder="Email Address"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <input
+          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <div className="text-center md:text-left">
+          <button
+            className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
+            type="submit">
+            Login
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LoginForm;
