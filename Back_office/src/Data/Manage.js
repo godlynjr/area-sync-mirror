a23fd1d55fa2b7052397a7c42459d2edf1fee140
Jean Luc <jlahoyo@asin.bj>
2024-01-04 09:28:54 +0100

    [feat]: Data class added and login request implemented

---
+++ Back_office/src/Data/Manage.js
@@ -0,0 +1,53 @@
+const api = "http://localhost:8080";
+
+class Data {
+    #accesToken = undefined;
+
+    constructor() {
+    }
+
+    #fillRequestHeaders(Format)
+    {
+      return {
+        accept: Format,
+        'Content-Type': 'application/json',
+        'Authorization' : 'Bearer ' + localStorage.getItem('authToken')
+      };
+    }
+
+    async login(mail, password) {
+        try {
+          const response = await fetch(api + "/loginn", {
+            method: "POST",
+            headers: this.#fillRequestHeaders(),
+            body: JSON.stringify({ email: mail, password: password }),
+          })
+          .then(response => response.json())
+          .then(data => {
+            localStorage.setItem('authToken', data.token);
+    
+            // Redirection
+            window.location.href = '/Dashboard';
+          })
+          .catch(error => {
+            console.error('Erreur de connexion :', error);
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+    searchUserByName(UserList, name)
+    {
+        const lowerName = name.toLowerCase();
+
+        return UserList.filter((item) => {
+        return item.username.toLowerCase() === lowerName || item.email.toLowerCase() === lowerName;
+        });
+    }
+
+    // Ajoutez d'autres méthodes pour gérer d'autres actions ici
+}
+
+const Infos = new Data;
+export default Infos;
