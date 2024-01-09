4ca561e312600f89e81b6b084ca7be2c0004ce02
Yannick <yannick.keke@epitech.eu>
2024-01-09 10:08:30 +0100

    [feat]: started with the second Area for discord service

--- Server/Services/Discord/index.js
+++ Server/Services/Discord/index.js
@@ -2,6 +2,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const config = require("./config.json");
 const fetch = require('node-fetch');
 const DiscordUser = require('../../models/DiscordUser');
+const airtable = require('airtable');
 
 const client = new Client({
     intents: [
@@ -9,11 +10,15 @@ const client = new Client({
       GatewayIntentBits.GuildMessages,
     ]
 });
+
+let DiscordIsActive = false;
+let AirtableIsActive = false;
+
 client.login(config.BOT_TOKEN);
 
 const login = (req, res) => {
     try {
-        const url = 'https://discord.com/api/oauth2/authorize?client_id=1186857028119973959&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fusers%2Fdiscord%2Fcallback&scope=identify';
+        const url = 'https://discord.com/api/oauth2/authorize?client_id=1186857028119973959&permissions=8&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fusers%2Fdiscord%2Fcallback&scope=identify+messages.read+bot+guilds.members.read';
         res.redirect(url);
     } catch (error) {
         console.error(error);
@@ -74,4 +79,91 @@ const callback = async (req, res) => {
     });
 };
 
-module.exports = { login, callback };
+// ---------------------------------------------------------------------------------------------
+// Area One
+
+// Écoutez l'événement 'messageUpdate'
+client.on('messageUpdate', (oldMessage, newMessage) => {
+    // Vérifiez si le message est épinglé
+    if (newMessage.pinned && !oldMessage.pinned) {
+        // Déclenchez une action lorsque le message est épinglé
+        console.log('Un message a été épinglé :', newMessage.content);
+
+        const eventDetails = {
+            // remplissez les détails de l'événement ici
+        };
+        // appelez votre fonction pour créer un événement Google Calendar ici
+        // createGoogleCalendarEvent(eventDetails);
+
+    }
+});
+
+async function createGoogleCalendarEvent(eventDetails) {
+    const {google} = require('googleapis');
+    const calendar = google.calendar({version: 'v3', auth});
+    const event = {
+        // remplissez les détails de l'événement ici
+    };
+    try {
+        const response = await calendar.events.insert({
+            auth: auth,
+            calendarId: 'primary',
+            resource: event,
+        });
+        console.log('Event created: %s', response.data.htmlLink);
+    } catch (error) {
+        console.log('There was an error contacting the Calendar service: ' + error);
+    }
+}
+
+// ---------------------------------------------------------------------------------------------
+
+// Area Two
+
+console.log('AirtableIsActive:', AirtableIsActive);
+
+const Airtableconnect = async (req, res) => {
+    try {
+        AirtableIsActive = true;
+        res.status(200).json({ message: 'Airtable is connected' });
+    } catch (error) {
+        console.error('Error in Airtableconnect:', error);
+        res.status(500).json({ message: 'Server error', error: error.toString() } , { message: 'Airtable is not connected' });
+    }
+};
+
+console.log('AirtableIsActive:', AirtableIsActive);
+
+const base = new airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+
+client.on('guildMemberAdd', member => {
+    if (!member.guild.me.hasPermission('VIEW_AUDIT_LOG')) {
+        console.log('The bot does not have the permission to view the audit log.');
+        return;
+    }
+
+    console.log('The bot has the permission to view the audit log.');
+
+    console.log('Airtable is connected');
+    if (AirtableIsActive) {
+        base('Async').create([
+            {
+                "fields": {
+                    "Username": member.user.username,
+                    "Discord ID": member.user.id,
+                    "JoinDate": new Date().toISOString()
+                }
+            }
+        ], function (err, records) {
+            if (err) {
+                console.error('Error adding user to Airtable:', err);
+                return;
+            }
+            console.log('Added user to Airtable:', member.user.username);
+        });
+    } else {
+        console.log('Airtable is not connected');
+    }
+});
+
+module.exports = { login, callback, Airtableconnect };
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const {  login, callback } = require('../Services/Discord/index');
+const {  login, callback, Airtableconnect } = require('../Services/Discord/index');
 const GithubService = require('../Services/Github/index');
 
 // Discord API
 router.get('/discord/login', login);
 router.get('/discord/callback', callback);
+router.post('/discord/airtable/connect', Airtableconnect);
 
 // Github API
 const githubService = new GithubService();
