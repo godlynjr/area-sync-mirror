83721b3690e9806b17bb7a65699be5073f50546d
leanofGod <godlyn.kikissagbe@epitech.eu>
2024-01-10 06:34:52 +0100

    [feat]: working apk build but not completly merge yet

--- Mobile_Client/App.js
+++ Mobile_Client/App.js
@@ -1,3 +1,5 @@
+import { registerRootComponent } from 'expo';
+import {Text, AppRegistry} from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -22,33 +24,33 @@ import TestScreen from './TestScreen';
 
 const Stack = createNativeStackNavigator();
 
-const App = () => {
-  return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="LoginPage" screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="OnboardingPage" component={OnboardingPage} />
-          <Stack.Group screenOptions={{ presentation: 'modal' }}>
-            <Stack.Screen name="ConnexionPage" component={ConnexionPage} />
-          </Stack.Group>
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen name="AppContentScreen" component={AppContentScreen} />
-          <Stack.Screen name="TestScreen" component={TestScreen} />
-          <Stack.Screen name="Service/Github" component={Github} />
-          <Stack.Screen name="Service/Google" component={Google} />
-          <Stack.Screen name="Service/Calendar" component={Calendar} />
-          <Stack.Screen name="Service/Notion" component={Notion} />
-          <Stack.Screen name="Service/Tally" component={Tally} />
-          <Stack.Screen name="Service/Spotify" component={Spotify} />
-          <Stack.Screen name="Service/Discord" component={Discord} />
-          <Stack.Screen name="Profil" component={Profil} />
-          <Stack.Screen name="Edit_profil" component={Edit_profil} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
-
-  );
+export default App = () => {
+    return (
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="LoginPage" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="OnboardingPage" component={OnboardingPage} />
+            <Stack.Group screenOptions={{ presentation: 'modal' }}>
+              <Stack.Screen name="ConnexionPage" component={ConnexionPage} />
+            </Stack.Group>
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+            <Stack.Screen name="AppContentScreen" component={AppContentScreen} />
+            <Stack.Screen name="TestScreen" component={TestScreen} />
+            <Stack.Screen name="Service/Github" component={Github} />
+            <Stack.Screen name="Service/Google" component={Google} />
+            <Stack.Screen name="Service/Calendar" component={Calendar} />
+            <Stack.Screen name="Service/Notion" component={Notion} />
+            <Stack.Screen name="Service/Tally" component={Tally} />
+            <Stack.Screen name="Service/Spotify" component={Spotify} />
+            <Stack.Screen name="Service/Discord" component={Discord} />
+            <Stack.Screen name="Profil" component={Profil} />
+            <Stack.Screen name="Edit_profil" component={Edit_profil} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    );
 };
 
-export default App;
+// AppRegistry.registerComponent('MDN', () => App);
+// registerRootComponent(App);
