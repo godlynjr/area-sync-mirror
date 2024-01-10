a26b4ad5df3c3fcf42d3e5273c4f4d11db723dc3
Jean Luc <jlahoyo@asin.bj>
2024-01-10 08:40:24 +0100

    [feat]: EditUserModal added on UserList screen

--- Back_office/src/Screens/UserList.js
+++ Back_office/src/Screens/UserList.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from "react";
 import Sidebar from "../Components/Sidebar";
 import NavBar from "../Components/NavBar";
 import DataTable from 'react-data-table-component';
-import Button from "../Components/Button";
 import Search from "../Components/Search";
 import Infos from "../Data/Manage.js";
+import EditUserModal from "../Components/EditUserModal";
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
-    const [searchInput, setSearchInput] = useState('');
     const columns = [
         {
             name: 'Id',
@@ -23,11 +22,15 @@ const UserList = () => {
             selector: row => row.email
         },
         {
+            name: 'Password',
+            selector: row => row.password
+        },
+        {
             name: 'Actions',
             cell: row => (
-                <div>
-                    <Button bg={"blue"} text={"Edit"} color={"white"} width={"60px"} height={"30px"} onClick={() => handleEdit(row._id)} />
-                    <Button bg={"red"} text={"Delete"} color={"white"} width={"60px"} height={"30px"} onClick={() => handleDelete(row._id)}/>
+                <div className="flex gap-2">
+                    <a className="cursor-pointer" onClick={() => handleEdit(row._id, row.username, row.email, row.password)}><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg></a>
+                    <a className="cursor-pointer" onClick={() => handleDelete(row._id)}><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></a>
                 </div>
             )
         },
@@ -37,7 +40,6 @@ const UserList = () => {
             try {
                 const data = await Infos.getUsers();
                 setUsers(data);
-                console.log("Reponsee", data);
             } catch (error) {
                 console.error("Error fetching users:", error);
             }
@@ -70,7 +72,9 @@ const UserList = () => {
         },
     };
 
-    const handleEdit = (userId) => {
+    const handleEdit = (userId, username, email, password) => {
+        setSelectedUser({ userId, username, email, password });
+        setEditModalOpen(true);
         console.log("Edit user with ID:", userId);
     };
 
@@ -80,11 +84,13 @@ const UserList = () => {
         console.log("Status: ", success);
     };
 
-    const handleSearchInputChange = (e) => {
-        setSearchInput(e.target.value);
-    };
+    const [isEditModalOpen, setEditModalOpen] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
 
-    const filteredUsers = searchInput !== '' ? Infos.searchUserByName(users, searchInput) : users;
+    const handleEditModalClose = () => {
+        setEditModalOpen(false);
+        setSelectedUser(null);
+    };
 
     return (
         <div className="flex">
@@ -97,11 +103,11 @@ const UserList = () => {
                     <h1 className="sm:text-[27px] md:text-[29px] text-[31px] text-black-900 text-shadow-ts font-bold font-roboto">
                         List of Users
                     </h1>
-                    <Search onChange={handleSearchInputChange} />
+                    <Search />
                     <div className="container mt-5 rounded-lg border-2 border-gray-300">
                         <DataTable
                             columns={columns}
-                            data={filteredUsers}
+                            data={users}
                             pagination
                             paginationPerPage={8}
                             paginationRowsPerPageOptions={[8]}
@@ -109,6 +115,11 @@ const UserList = () => {
                         />
                     </div>
                 </div>
+                {selectedUser && <EditUserModal
+                    isOpen={isEditModalOpen}
+                    onClose={handleEditModalClose}
+                    user={selectedUser}
+                />}
             </div>
         </div>
     );
