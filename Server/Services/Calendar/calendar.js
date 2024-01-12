2be1892c77990260530cb00773616fe55582354a
laurence090403 <bridge.atindokpo@epitech.eu>
2024-01-12 17:27:45 +0100

    feat: separation of web client notion

--- Server/Services/Calendar/calendar.js
+++ Server/Services/Calendar/calendar.js
@@ -34,8 +34,12 @@ const channel = {
     type: 'web_hook',
     address: 'https://bird-distinct-gator.ngrok-free.app/users/calendar/google-calendar-webhook',
 };
+
 const scopes = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events'];
+
 const token = "";
+
+
 const googled = async (req, res) => {
     const url = oauth2Client.generateAuthUrl({
         access_type: 'offline',
@@ -45,19 +49,28 @@ const googled = async (req, res) => {
     res.redirect(url);
 };
 
+async function getUserInfo() {
+    const plus = google.plus({ version: 'v1', auth: oauth2Client });
+    const userInfo = await plus.people.get({ userId: 'me' });
+    console.log('User Info:', userInfo.data);
+    return userInfo.data;
+}
+
 const callbacks = async (req, res) => {
     const code = req.query.code;
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
+    console.log(tokens);
+    // Récupérer les informations du profil utilisateur
+    // const userInfo = await getUserInfo();
 
     res.send({
-        msg: "Successfully loggeed in to Google Calendar",
-    }); 
+        msg: "Successfully logged in to Google Calendar",
+        // userInfo: userInfo,
+    });
 };
 
-
 const callback_calendar = async (req, res) => {
-    
     // calendar.events.watch({
     //     calendarId: calendarId,
     //     requestBody: {
@@ -66,6 +79,7 @@ const callback_calendar = async (req, res) => {
     //         address: channel.address,
     //     },
     // });
+
     const response = await calendar.events.list({
         auth: oauth2Client,
         calendarId: 'primary', // 'primary' represents the user's primary calendar
@@ -75,34 +89,31 @@ const callback_calendar = async (req, res) => {
   
     const events = response.data.items;
     numbers = events.length;
-    console.log(`Nombre d'événements à venir : ${numbers}`);
-
     return res.status(200).end();
 };
 
+
 const calendarwebhook = async (req, res) => {
     try {
-        // const eventId = JSON.stringify(req.headers);
-        // Traitez les changements (nouvel événement, mise à jour, suppression, etc.)
-        // Vous pouvez extraire les détails de l'événement de `changes`
-
         const response = await calendar.events.list({
             auth: oauth2Client,
             calendarId: 'primary', // 'primary' represents the user's primary calendar
             singleEvents: true,
             orderBy: 'startTime',
           });
-      
+
         const events = response.data.items;
+        console.log(`events lenght  : ${events.length}`);
+        console.log(`numbers avant : ${numbers}`);
         if (events.length > numbers) {
-            const newEvent = events[events.length - 1];
-
-            // Display information about the new event
-            console.log('Newly added event:');
-            console.log('Title:', newEvent.summary);
-            console.log('Start:', newEvent.start.dateTime || newEvent.start.date);
-            console.log('End:', newEvent.end.dateTime || newEvent.end.date);
-
+            events.sort((a, b) => new Date(b.created) - new Date(a.created));
+            // Récupération de l'événement le plus récent
+            const dernierEvenement = events[0];
+            console.log(`Événement le plus récent :`);
+            console.log(`Titre: ${dernierEvenement.summary}`);
+            console.log(`Début: ${dernierEvenement.start.dateTime || dernierEvenement.start.date}`);
+            console.log(`Fin: ${dernierEvenement.end.dateTime || dernierEvenement.end.date}`);
+            console.log(`Date de création: ${dernierEvenement.created}`);
 
             // Ajoutez ici le code pour créer un événement dans Notion
             // en utilisant les informations reçues de l'événement Google Calendar.
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const {  login, callback } = require('../Services/Discord/index');
+const {  notion_log, notion_callback } = require('../Services/Notion/notion');
 const {  googled, callbacks, calendarwebhook, callback_calendar } = require('../Services/Calendar/calendar');
 
 router.get('/discord/login', login);
@@ -11,4 +12,6 @@ router.get('/calendar/callback', callbacks);
 router.get('/calendar/watch', callback_calendar);
 router.post('/calendar/google-calendar-webhook', calendarwebhook);
 
+router.get('/calendar/notion/login', notion_log);
+router.get('/calendar/notion/callback', notion_callback);
 module.exports = router
