59d77497d7a15506857715e1cd23d078214c208c
Jean Luc <jlahoyo@asin.bj>
2024-01-12 04:35:28 +0100

    [feat]: Youtube services routes added to users.js

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const {  login, callback, Airtableconnect } = require('../Services/Discord/index');
 const GithubService = require('../Services/Github/index');
 const {  googled, callbacks, calendarwebhook, callback_calendar } = require('../Services/Calendar/calendar');
+const { loginyt} = require('../Services/Youtube/youtube');
 
 // Discord API
 router.get('/discord/login', login);
@@ -13,6 +14,11 @@ router.get('/calendar/login', googled);
 router.get('/calendar/callback', callbacks);
 router.get('/calendar/watch', callback_calendar);
 router.post('/calendar/google-calendar-webhook', calendarwebhook);
+
+// Youtube API
+router.get('/youtube/login', loginyt);
+// router.get('/youtube/callback', Callback);
+// router.post('/youtube/discord/connect', connect_yt_discord);
 
 // Github API
 const githubService = new GithubService();
