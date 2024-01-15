5602a4f51d268cbd5bea3795be57e0ae967dfd3e
Jean Luc <jlahoyo@asin.bj>
2024-01-15 22:09:12 +0100

    [feat]: youtubeXdrive request added on users route

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,16 +1,16 @@
 const express = require("express")
 const router = express.Router()
-const {  login, callback, Airtableconnect, CalendarConnect } = require('../Services/Discord/index');
+// const {  login, callback, Airtableconnect, CalendarConnect } = require('../Services/Discord/index');
 const {  notion_log, notion_callback } = require('../Services/Notion/notion');
 const GithubService = require('../Services/Github/index');
 const {  googled, callbacks, calendarwebhook, callback_calendar } = require('../Services/Calendar/calendar');
-const { loginyt, Callback, youtubeXgmail } = require('../Services/Youtube/youtube');
+const { loginyt, Callback, youtubeXgmail, youtubeXdrive, youtubeXspotify } = require('../Services/Youtube/youtube');
 
 // Discord API
-router.get('/discord/login', login);
-router.get('/discord/callback', callback);
-router.post('/discord/airtable/connect', Airtableconnect);
-router.post('/discord/calendar/connect', CalendarConnect);
+// router.get('/discord/login', login);
+// router.get('/discord/callback', callback);
+// router.post('/discord/airtable/connect', Airtableconnect);
+// router.post('/discord/calendar/connect', CalendarConnect);
 
 // Calendar API
 router.get('/calendar/login', googled);
@@ -22,6 +22,8 @@ router.post('/calendar/google-calendar-webhook', calendarwebhook);
 router.get('/youtube/login', loginyt);
 router.get('/youtube/callback', Callback);
 router.post('/youtube/gmail/connect', youtubeXgmail);
+router.post('/youtube/drive/connect', youtubeXdrive);
+router.post('/youtube/spotify/connect', youtubeXspotify);
 
 // Github API
 const githubService = new GithubService();
