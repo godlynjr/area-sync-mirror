41b31d38654d3356ab131017f99e832ee61739e3
godlynjr <godlyn.kikissagbe@epitech.eu>
2024-01-16 01:08:29 +0100

    [feat]: uncomment date&time routers

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -48,8 +48,8 @@ router.get('/spotify/login', ConnectSpotify);
 router.get('/spotify/callback', SpotifyCallback);
 router.post('/spotify/connect', createPlaylistWithLikedSongs)
 // Date&Time
-// router.get('/datetime/sendmotivation', scheduleEmails);
-// router.get('/datetime/sendweather', sendWeatherEmails);
-// router.get('/datetime/sendprayertime', definePrayerTime);
+router.get('/datetime/sendmotivation', scheduleEmails);
+router.get('/datetime/sendweather', sendWeatherEmails);
+router.get('/datetime/sendprayertime', definePrayerTime);
 
 module.exports = router
